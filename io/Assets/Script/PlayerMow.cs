5f134c0ea8575aee864b93bfb0b9734f88ee697c
Dania-mdn <58430901+Dania-mdn@users.noreply.github.com>
2024-12-15 10:26:29 +0200

    asfaf
    asfasf

---
+++ io/Assets/Script/PlayerMow.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMow : NetworkBehaviour
+{
+    private RaycastHit hit;
+    private Ray ray;
+    public GameObject positionMous;
+    private Rigidbody rb;
+
+    public GameObject Bullet;
+    public GameObject StartPosition;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit);
+
+        positionMous.transform.position = hit.point;
+
+        if (!isLocalPlayer) return;
+        Vector3 direction = positionMous.transform.position - transform.position;
+        direction.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+
+        rb.velocity = transform.forward * 8;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(Bullet, StartPosition.transform.position, Quaternion.identity);
+            newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 40, ForceMode.Impulse);
+        }
+    }
+}
