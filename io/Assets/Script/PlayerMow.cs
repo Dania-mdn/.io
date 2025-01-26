bb25abdc21bf84c520252d444be78acb14c1dcf3
Dania-mdn <58430901+Dania-mdn@users.noreply.github.com>
2025-01-26 17:44:40 +0200

    asdfsaf
    asfasf

--- io/Assets/Script/PlayerMow.cs
+++ io/Assets/Script/PlayerMow.cs
@@ -13,6 +13,9 @@ public class PlayerMow : NetworkBehaviour
     public GameObject Bullet;
     public GameObject StartPosition;
 
+    private int coldawn = 1;
+    private float time = 0;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -29,16 +32,23 @@ public class PlayerMow : NetworkBehaviour
         Vector3 direction = positionMous.transform.position - transform.position;
         direction.y = 0;
 
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+        rb.velocity = transform.forward * 8;
+
+        if(time > 0)
+        {
+            time = time - Time.deltaTime;
+        }
+
         if (Input.GetMouseButton(0))
         {
-            Quaternion rotation = Quaternion.LookRotation(direction);
-            transform.rotation = rotation;
-            rb.velocity = transform.forward * 8;
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            GameObject newBullet = Instantiate(Bullet, StartPosition.transform.position, Quaternion.identity);
-            newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 40, ForceMode.Impulse);
+            if (time <= 0)
+            {
+                GameObject newBullet = Instantiate(Bullet, StartPosition.transform.position, Quaternion.identity);
+                newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 40, ForceMode.Impulse);
+                time = coldawn;
+            }
         }
     }
 }
